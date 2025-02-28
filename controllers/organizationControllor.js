7e354fffc248dfdf7d38d157de993b3a6ff7dc25
dap9248 <dap9248@rit.edu>
2025-02-27 20:28:10 -0500

    added an organization controller with a function to find organization by ID, added organizationSanitization to santize and validate user organization fields

---
+++ controllers/organizationControllor.js
@@ -0,0 +1,10 @@
+const { Organization, Sequelize } = require("../models");
+
+//searches for an organization by primary key
+//if no organization found it will return a null value
+exports.getOrganization = async (req, res) => {
+    const organizationID = req.body;
+    const organization = await Organization.FindByPK(organizationID);
+    return organization
+
+}
