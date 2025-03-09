96c3cfa3dfa8df90c5eeec66abfbe8afd12a81aa
dap9248 <dap9248@rit.edu>
2025-03-09 19:26:47 -0400

    added eventControllor, wrote out comments for a function to create events, and to pull a list of evenTypes from both default and organization

---
+++ controllers/eventController.js
@@ -0,0 +1,9 @@
+const {Organization, Event, EventGroup, EventStaff, Attendee, EventTypes, OrganizationEventTypes, DefaultEventTypes} = require("../models");
+
+async function createEvent(userId, name ){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
