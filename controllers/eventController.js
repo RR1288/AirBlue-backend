a469069bb8ee14143528ec9d68b4395313da5a6a
dap9248 <dap9248@rit.edu>
2025-03-09 20:45:38 -0400

    finished the event creation function and added the event service file

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,9 +1,50 @@
-const {Organization, Event, EventGroup, EventStaff, Attendee, EventTypes, OrganizationEventTypes, DefaultEventTypes} = require("../models");
+const { Organization, Event, EventGroup, EventStaff, Attendee, EventTypes, OrganizationEventTypes, DefaultEventTypes, sequelize } = require("../models");
 
-async function createEvent(userId, name ){
+/*
+CREATE EVENT
+this function
+*/
+async function createEvent(userId, name, startDate, endDate, description, typeID, organizationID) {
     try {
-        
+        let event;
+        await sequelize.transaction(async t => {
+            //create the event
+            event = await Event.create({
+                EventID: event.EventID,
+                EventName: name,
+                EventStartDate: startDate,
+                EventEndDate: endDate,
+                EventDescription: description,
+                TyperID: typeID,
+                OrganizationID: organizationID
+            });
+            //add the creating user to the event staff as an eventplanner
+            const eventStaff = await EventStaff.create({
+                UserID: userId,
+            });
+        });
+        //returns the event Id on success
+        return event.EventID
     } catch (error) {
-        
+
     }
+}
+
+/*
+get event Types:
+returns a list of event types that the user can assign to a event
+*/
+async function getEventTypes(organizationID) {
+    try {
+        await sequelize.transaction(async t => {
+            //get a list of all default eventTypes
+
+            // get a list of all organization eventTypes that belong to the current organization and append it to the prior list
+
+            //return the list generated
+        });
+    } catch (error) {
+
+    }
+
 }
