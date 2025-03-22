09dbd22590f6dec9d1caa03de052ac27a4ac00c3
dap9248 <dap9248@rit.edu>
2025-03-22 01:53:21 -0400

    finished up on a view that gets all attendees in a specific event for an event planner'

--- views/eventPlannerViews.js
+++ views/eventPlannerViews.js
@@ -1,4 +1,4 @@
-const { Sequelize, Attendee, Itinerary, Invititation, User } = require('../models');
+const { Sequelize, Attendee, Itinerary, Invititation, User, EventGroup } = require('../models');
 
 //simple call to get all attendees for a specific event. This will also include their status from itinerary if they are pending approval
 exports.getAttendees = async (eventID) => {
@@ -20,9 +20,13 @@ exports.getAttendees = async (eventID) => {
                 {
                     model: Itinerary,
                     attributes: [['ApprovalStatus', 'status'], ['TotalCost', 'cost']],
-                    required: true,
                     where: { ApprovalStatus: 'pending' }//could be a sticking point keep in mind for manual test
-                }
+                },
+                {
+                    model: EventGroup,
+                    attributes: [['Name', 'name'], ['FlightBudget', 'budget']],
+                    required: true,
+                },
 
             ],
             where: { EventID: eventID }
@@ -31,12 +35,19 @@ exports.getAttendees = async (eventID) => {
         //making it so that I am only returning the information that I want
         for (let i = 0; i < attendees.length; i++) {
             let combinedName = attendees[i].dataValues.User.firstName + attendees[i].dataValues.User.lastName;
+            //checking to make sure there are actual values for bookingCost and status in Itinerary
+            let bookingCost = attendees[i].dataValues.Itinerary.cost;
+            if (!bookingCost) bookingCost = null;
+            let status = attendees[i].dataValues.Itinerary.status;
+            if (!status) status = null;
             results.push(
                 {
                     'Name': combinedName,
                     'email': attendees[i].dataValues.User.email,
-                    'status': attendees[i].dataValues.Itinerary.status,
-                    'bookingCost': attendees[i].dataValues.Itinerary.cost
+                    'status': status,
+                    'bookingCost': bookingCost,
+                    'groupName': attendees[i].dataValues.EventGroup.name,
+                    'budget': attendees[i].dataValues.EventGroup.budget, 
                 });
         }
         return results;
