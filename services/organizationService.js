b6cf50b59dfda7e74b5a0d0c5c9af088ca922d39
dap9248 <dap9248@rit.edu>
2025-03-25 16:11:20 -0400

    created an organization service files that for now are pulling the organization views. This can later be adjusted to include all of the other pure organization functions

---
+++ services/organizationService.js
@@ -0,0 +1,29 @@
+const {sendSuccess, sendError} = require("../utils/responseHelpers");
+const OrganizationViews = require('../views/organizationViews');
+const {validateOrganizationID} = require('../utils/OrganizationSanitization');
+
+exports.getOrganizationUsers = async (req, res) =>{
+    try {
+        let organizationId = req.user.OrganizationID;
+        if (!(await validateOrganizationID(organizationId))) return sendError(res, 'invalid organizationId', 400);
+        const users = await OrganizationViews.getOrganizationUsers(organizationId);
+        if (!users) return sendError(res, 'failed to get organization Users', 400);
+
+        return sendSuccess(res, 'successfully retrieved organization users', users);
+    } catch (error) {
+        return sendError(res, 'failed to get organization Users');
+    }
+};
+
+exports.getOrganizationInfo = async (req, res) =>{
+    try {
+        let organizationId = req.user.OrganizationID;
+        if (!(await validateOrganizationID(organizationId))) return sendError(res, 'invalid organizationId', 400);
+        const users = await OrganizationViews.getOrganizationInfo(organizationId);
+        if (!users) return sendError(res, 'failed to get organization info', 400);
+
+        return sendSuccess(res, 'successfully retrieved organization info', users);
+    } catch (error) {
+        return sendError(res, 'failed to get organization info');
+    }
+};
