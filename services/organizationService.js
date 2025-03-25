3dc552289ef0b7f4c1056cb586b0636a59157526
dap9248 <dap9248@rit.edu>
2025-03-25 17:02:16 -0400

    got routes up need to manual test them

--- services/organizationService.js
+++ services/organizationService.js
@@ -4,24 +4,29 @@ const {validateOrganizationID} = require('../utils/OrganizationSanitization');
 
 exports.getOrganizationUsers = async (req, res) =>{
     try {
-        let organizationId = req.user.OrganizationID;
+        let organizationId = parseInt(req.user.OrganizationID);
+        //validation
         if (!(await validateOrganizationID(organizationId))) return sendError(res, 'invalid organizationId', 400);
+        //run function
         const users = await OrganizationViews.getOrganizationUsers(organizationId);
         if (!users) return sendError(res, 'failed to get organization Users', 400);
-
+        //return on success
         return sendSuccess(res, 'successfully retrieved organization users', users);
     } catch (error) {
+        console.log(error);
         return sendError(res, 'failed to get organization Users');
     }
 };
 
 exports.getOrganizationInfo = async (req, res) =>{
     try {
-        let organizationId = req.user.OrganizationID;
+        let organizationId = parseInt(req.user.OrganizationID);
+        //validation
         if (!(await validateOrganizationID(organizationId))) return sendError(res, 'invalid organizationId', 400);
+        //run function
         const users = await OrganizationViews.getOrganizationInfo(organizationId);
         if (!users) return sendError(res, 'failed to get organization info', 400);
-
+        //return on success
         return sendSuccess(res, 'successfully retrieved organization info', users);
     } catch (error) {
         return sendError(res, 'failed to get organization info');
