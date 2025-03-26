a5aa27e4e1c1400b28b4f0bd32c32fe951781460
dap9248 <dap9248@rit.edu>
2025-03-26 15:26:26 -0400

    created a function in the user controller to create an organization and add the owner as the first organization user with admin privileges

--- controllers/organizationControllor.js
+++ controllers/organizationControllor.js
@@ -1,5 +1,47 @@
 const { Organization, UserOrganization,Sequelize } = require("../models");
 
+
+/*
+creates an organization offered an ownerId, name, and description, 
+it the creates the organization and adds the owner in as an administrator for the organization
+*/
+exports.createOrganization = async (name, description, ownerId) =>{
+    try {
+        //start transaction
+        let organization;
+        await sequelize.transaction(async (t) => {
+            organization = await Organization.create({
+                OrganizationName: name,
+                Description: description,
+                Owner: ownerId,
+                IsActive: true,
+            },
+            {transaction: t}
+            );
+            let userOrganization = await UserOrganization.create({
+                UserID: ownerId,
+                OrganizationID: organization.OrganizationID,
+                Roles: 'A',
+                StillActive: true,
+            },
+            {transaction: t}
+            );
+        });
+        if (!organization) throw new Error('could noot make organization');
+        return true;//on success just return true to tell the use that they have made an org as otherwise you may be returning to much info
+    } catch (error) {
+        throw new Error('failed to create organization');
+    }
+};
+
+
+
+
+/*
+
+GET FUNCTIONS
+- all of these will likely need to get moved to views eventually
+*/
 //searches for an organization by primary key
 //if no organization found it will return a null value
 exports.getOrganization = async (organizationID) => {
