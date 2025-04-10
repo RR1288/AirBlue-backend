277fa5fada08edf662a5f35384d4164139720875
dap9248 <dap9248@rit.edu>
2025-04-09 21:58:59 -0400

    fixed issue with the new changes to the protect function when setting up 2fa so made a simpler version for it that maintains security but doesn't block the 2fa setup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -30,3 +30,29 @@ exports.protect = async (req, res, next) => {
     }
 };
 
+exports.protectOnSetup = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) return sendError(res, "Not authorized, token missing", 401);
+
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("decoded: ", decoded);
+        
+        // Fetch user login info from UserLogin table
+        const user = await UserLogin.findByPk(decoded.id);
+        if (!user) return sendError(res, "User not found", 404);
+        user.roles = decoded.roles; // Attach roles to user
+        user.username = decoded.username;   // Attach username to user
+        user.id = decoded.id;
+        user.OrganizationID = decoded.OrganizationID;
+        req.user = user; // Attach user to request
+
+        
+        next(); // Move to the next function in the route handler
+    } catch (err) {
+        console.error(err);
+        return sendError(res, "Not authorized, token failed", 401);
+    }
+};
+
